624190f018f02b0ffe370af7a9961016ebd91713
Victor <65656020+Chat-mots@users.noreply.github.com>
2022-04-26 15:51:07 +0200

    ajout de "getpseudo"

--- sql/main.py
+++ sql/main.py
@@ -24,24 +24,24 @@ def creerjoueur(pseudo):
     sql_badge = "INSERT INTO badges (ID) VALUES (%s);"
     # on insere dans la table base
     mycursor.execute(sql, pseudo)
-    id = (mycursor.lastrowid,)
+    idjoueur = (mycursor.lastrowid,)
     # on insere dans les autres tables
-    mycursor.execute(sql_badge, id)
-    mycursor.execute(sql_stat, id)
+    mycursor.execute(sql_badge, idjoueur)
+    mycursor.execute(sql_stat, idjoueur)
 
     mydb.commit()
 
 
-def ajoutermort(nbrmort, id):
+def ajoutermort(nbrmort, idjoueur):
     """ajoute le nombre de morts de la partie précedente au joueur
 
 
     :param int nbrmort: entier >= 0
-    :param id: id du joueur (entier à 6 chiffres)
+    :param idjoueur: id du joueur (entier à 6 chiffres)
     :return: None
     """
     # on récupere le nombre de morts précedentes
-    id_sp = (id,)
+    id_sp = (idjoueur,)
     sqlcall = "SELECT `nombre_morts` FROM `statistiques` WHERE `ID` = %s;"
     mycursor.execute(sqlcall, id_sp)
     morts_prec = mycursor.fetchall()
@@ -50,7 +50,7 @@ def ajoutermort(nbrmort, id):
     nbrmort += int(morts_prec)
 
     # on remplace par le nouveau nombre de morts
-    valeurs = (nbrmort, id)
+    valeurs = (nbrmort, idjoueur)
     mycursor.execute("SELECT * FROM `statistiques`;")
     sql = "UPDATE `statistiques` SET `nombre_morts` = '%s' WHERE `statistiques`.`ID` = %s"
     mycursor.execute(sql, valeurs)
@@ -63,17 +63,17 @@ def ajoutermort(nbrmort, id):
     mydb.commit()
 
 
-def ajoutertemps_de_jeu(id, temps):
+def ajoutertemps_de_jeu(idjoueur, temps):
     """ajoute le temps de jeu du joueur à ses statistiques
 
     ajoute au joueur le temps qu'il a passé à effectuer la partie (**elle s'additionne à ses temps précédents**)
 
-    :param int id: id du joueur (entier à 6 chiffres)
+    :param int idjoueur: id du joueur (entier à 6 chiffres)
     :param int temps: nombre de secondes
     :return: None
     """
     # on récupère le temps de jeu précedent
-    id_sp = (id,)
+    id_sp = (idjoueur,)
     sqlcall = "SELECT `temps_de_jeu` FROM `statistiques` WHERE `ID` = %s;"
     mycursor.execute(sqlcall, id_sp)
     temps_prec = mycursor.fetchall()
@@ -81,7 +81,7 @@ def ajoutertemps_de_jeu(id, temps):
     temps += int(temps_prec)
 
     # on remplace par le nouveau temps de jeu
-    valeurs = (temps, id)
+    valeurs = (temps, idjoueur)
     mycursor.execute("SELECT * FROM `statistiques`;")
     sql = "UPDATE `statistiques` SET `temps_de_jeu` = '%s' WHERE `statistiques`.`ID` = %s"
     mycursor.execute(sql, valeurs)
@@ -89,39 +89,39 @@ def ajoutertemps_de_jeu(id, temps):
     mydb.commit()
 
 
-def modifierelo(id, elo):
+def modifierelo(idjoueur, elo):
     """modifie l'elo d'un joueur
 
     après avoir calculé l'elo à l'aide de la fonction de calcul (faite autre part), `modifierelo` met à jour l'elo du
     joueur dans la base de données.
 
-    :param int id: id du joueur (entier à 6 chiffres)
+    :param int idjoueur: id du joueur (entier à 6 chiffres)
     :param int elo: elo calcul, entier >= 0
     :return: None
     """
     # on récupère l'elo précedent
-    eloprec = getelo(id)
+    eloprec = getelo(idjoueur)
     eloprec = int(eloprec)
     elo += eloprec
 
     # si le nouvel elo devient inférieur à 0, on le remets à 0
     if eloprec + elo < 0:
         print("inférieur à 0")
-        id_tuple = (id,)
+        id_tuple = (idjoueur,)
         sqlbas = "UPDATE `base` SET `ELO` = '0' WHERE `base`.`ID` = %s"
         mycursor.execute(sqlbas, id_tuple)
         mydb.commit()
 
     # on ajoute le nouvel elo
-    valeurs = (elo, id)
+    valeurs = (elo, idjoueur)
     mycursor.execute("SELECT * FROM `base`;")
     sql = "UPDATE `base` SET `ELO` = '%s' WHERE `base`.`ID` = %s"
     mycursor.execute(sql, valeurs)
     mydb.commit()
 
     # on vérifie si il s'agit d'un nouveau record
-    gethautelo = ("SELECT `plus_haut_ELO` FROM `statistiques` WHERE `ID` = %s;")
-    mycursor.execute(gethautelo, (id,))
+    gethautelo = "SELECT `plus_haut_ELO` FROM `statistiques` WHERE `ID` = %s;"
+    mycursor.execute(gethautelo, (idjoueur,))
     haut_elo = mycursor.fetchall()[0][0]
     if elo > haut_elo:
         sqlhaut = "UPDATE `statistiques` SET `plus_haut_ELO` = '%s' WHERE `statistiques`.`ID` = %s"
@@ -129,14 +129,29 @@ def modifierelo(id, elo):
         mydb.commit()
 
 
-def getelo(id):
+def getpseudo(idjoueur):
+    """récupere le pseudo d'un joueur d'id `id`
+
+    :param int idjoueur:
+    :return:
+    """
+    id_sp = (idjoueur,)
+    sqlcall = "SELECT `pseudo` FROM `base` WHERE `ID` = %s;"
+    mycursor.execute(sqlcall, id_sp)
+    pseudojoueur = mycursor.fetchall()
+    pseudojoueur = pseudojoueur[0][0]
+
+    return pseudojoueur
+
+
+def getelo(idjoueur):
     """récupere l'elo d'un joueur d'id `id`
 
 
-    :param int id: id du joueur (nombre à 6 chiffres)
+    :param int idjoueur: id du joueur (nombre à 6 chiffres)
     :return: int correspondant à l'elo du joueur
     """
-    id_sp = (id,)
+    id_sp = (idjoueur,)
     sqlcall = "SELECT `ELO` FROM `base` WHERE `ID` = %s;"
     mycursor.execute(sqlcall, id_sp)
     eloprec = mycursor.fetchall()
@@ -145,15 +160,15 @@ def getelo(id):
     return eloprec
 
 
-def changerpseudo(id, nouveaupseudo):
+def changerpseudo(idjoueur, nouveaupseudo):
     """ change le pseudo d'un joueur d'id `id`
 
 
-    :param int id: id du joueur (nombre à 6 chiffres)
+    :param int idjoueur: id du joueur (nombre à 6 chiffres)
     :param str nouveaupseudo: le nouveau pseudo du joueur
     :return: None
     """
-    valeurs = (nouveaupseudo, id)
+    valeurs = (nouveaupseudo, idjoueur)
     mycursor.execute("SELECT * FROM `base`;")
     sql = "UPDATE `base` SET `pseudo` = %s WHERE `base`.`ID` = %s"
     mycursor.execute(sql, valeurs)
@@ -161,26 +176,26 @@ def changerpseudo(id, nouveaupseudo):
     mydb.commit()
 
 
-def serievictoire(id, resultat):
+def serievictoire(idjoueur, resultat):
     """gère les series de victoire à effectuer en fin de partie
 
     serievictoire gère les series de victoires à la fin de la partie, en fonction du classement du joueur.
     A partir de la position du joueur dans le classement, on ajoute 1 à sa série de victoire, ou on la remets à 0.
 
-    :param int id: id du joueur (entier à 6 chiffres)
+    :param int idjoueur: id du joueur (entier à 6 chiffres)
     :param int resultat: entier compris entre 1 et 8
     :return: None
     """
     sqlrecord = "SELECT `plus_grande_serie_de_victoire` from `statistiques` where `statistiques`.`id` = %s"
-    mycursor.execute(sqlrecord, (id,))
+    mycursor.execute(sqlrecord, (idjoueur,))
     record = mycursor.fetchall()[0][0]
     sql = "SELECT `serie_de_victoire_actuelle` from `statistiques` where `statistiques`.`id` = %s"
-    mycursor.execute(sql, (id,))
+    mycursor.execute(sql, (idjoueur,))
     serie_de_victoire = mycursor.fetchall()[0][0]
 
     if resultat <= 3:  # c'est une défaite
         # on ajoute 1 à la série de victoire
-        valeurs = (serie_de_victoire+1, id)
+        valeurs = (serie_de_victoire + 1, idjoueur)
         sqladd = "UPDATE `statistiques` SET `serie_de_victoire_actuelle` = '%s' WHERE `statistiques`.`ID` = %s;"
         mycursor.execute(sqladd, valeurs)
         mydb.commit()
@@ -192,7 +207,7 @@ def serievictoire(id, resultat):
             mydb.commit()
 
     else:  # c'est une défaite
-        valeurs = (0, id)
+        valeurs = (0, idjoueur)
         sqladd = "UPDATE `statistiques` SET `serie_de_victoire_actuelle` = '%s' WHERE `statistiques`.`ID` = %s;"
         mycursor.execute(sqladd, valeurs)
         mydb.commit()
